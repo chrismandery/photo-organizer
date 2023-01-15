90ebdb02112fcfd6d1f2260625f050aff12550ad
Christian Mandery <christian@mandery.ch>
2023-01-15 10:38:58 +0100

    Basic handling of commands and reading/writing the index file

--- src/index.rs
+++ src/index.rs
@@ -1,15 +1,39 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Write};
 use std::path::{Path, PathBuf};
 
-const INDEX_FILE_NAME: &str = "photo_organizer_index.csv";
+const INDEX_FILE_NAME: &str = "photo_organizer_index.json";
 
+#[derive(Deserialize, Serialize)]
+pub struct UserConfig {
+    file_naming_scheme: String
+}
+
+#[derive(Deserialize, Serialize)]
 pub struct IndexEntry {
     filepath: String,
     orig_filename: String,
     filehash: String  // TODO: Change type
 }
 
-pub type Index = Vec<IndexEntry>;
+#[derive(Deserialize, Serialize)]
+pub struct Index {
+    user_config: UserConfig,
+    photos: Vec<IndexEntry>
+}
+
+impl Default for Index {
+    fn default() -> Self {
+        Index {
+            user_config: UserConfig {
+                file_naming_scheme: String::from("%Y%m%d_%H%M%S_%{type}.%{fileextension}")  // TODO
+            },
+            photos: vec!()
+        }
+    }
+}
 
 /// Determines for a given directory (usually the working directory) both the path of the root directory (containing the index file) and
 /// the subdirectory (portion of the path from the root to the given directory). Returns Ok(None) is the given directory is not part of a
@@ -20,10 +44,19 @@ pub fn get_index_root_and_subdir(dir: &Path) -> Result<Option<(PathBuf, PathBuf)
 
 /// Reads the index file for given root directory.
 pub fn read_index_file(root_dir: &Path) -> Result<Index> {
-    todo!();
+    let filepath = root_dir.join(INDEX_FILE_NAME);
+    let file = File::open(&filepath).with_context(|| format!("Could not open index file at {} for reading!", filepath.display()))?;
+    let reader = BufReader::new(file);
+    let res = serde_json::from_reader(reader).with_context(|| format!("Could not parse index file at {}!", filepath.display()))?;
+    Ok(res)
 }
 
 /// Writes index file to given root directory.
 pub fn write_index_file(root_dir: &Path, index: &Index) -> Result<()> {
-    todo!();
+    let filepath = root_dir.join(INDEX_FILE_NAME);
+    let file = File::open(&filepath).with_context(|| format!("Could not open index file at {} for writing!", filepath.display()))?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(&mut writer, index)?;
+    writer.flush()?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use std::env::current_dir;
+use std::path::Path;
 
-use index::{get_index_root_and_subdir, write_index_file};
+use index::{Index, get_index_root_and_subdir, write_index_file};
 
+mod checks;
 mod collection;
 mod index;
 
@@ -29,33 +31,21 @@ enum Command {
     /// Show meta information for image files within the current directory
     List {
         #[arg(long, short)]
-        recursive: bool, 
+        recursive: bool
+    },
+
+    /// Renames the files in the current directory (and potentially subdirectories) to follow the configured naming scheme
+    Rename {
+        #[arg(long, short)]
+        recursive: bool
     },
 
     /// Update index file adding, renaming and deleting entries as image files have been changed
     Update
 }
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-
-    // Get photo collection that the current working directory is a part of (required by all commands expect init)
-    let root_dir = get_index_root_and_subdir(&current_dir()?)?;
-
-    if args.command == Command::Init {
-        match root_dir {
-            Some((root_dir, _)) => {
-                println!("Cannot initialize a new photo collection here!");
-                println!("This directory is already within the collection at: {}", root_dir.display());
-            },
-            None => {
-                let wd = current_dir()?;
-                write_index_file(&wd, &vec!())?;
-                println!("Index file created for directory {}.", wd.display());
-            }
-        }
-    }
-
+/// Handles execution of all commands except the init command.
+fn handle_command(args: &Args, root_dir: &Path, subdir: &Path) -> Result<()> {
     match args.command {
         Command::Check => {
             todo!();
@@ -64,9 +54,43 @@ fn main() -> Result<()> {
         Command::List { recursive: bool }=> {
             todo!();
         },
+        Command::Rename { recursive: bool }=> {
+            todo!();
+        },
         Command::Update => {
             todo!();
         }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    // Get photo collection that the current working directory is a part of (required by all commands expect init)
+    let found_collection = get_index_root_and_subdir(&current_dir()?)?;
+
+    if args.command == Command::Init {
+        // Specifically handle the init command since it is the only command that does not require an existing photo collection
+        match found_collection {
+            Some((ref root_dir, _)) => {
+                println!("Cannot initialize a new photo collection here!");
+                println!("This directory is already within the collection at: {}", root_dir.display());
+            },
+            None => {
+                let wd = current_dir()?;
+                write_index_file(&wd, &Index::default())?;
+                println!("Empty index file created for directory {}.", wd.display());
+                println!("Adjust configuration options in file if desired and then run the \"update\" command.");
+            }
+        }
+    } else if let Some((ref root_dir, ref subdir)) = found_collection {
+        // Handle all other commands
+        handle_command(&args, root_dir, subdir)?;
+    } else {
+        println!("Working directory does not seem to be part of a photo collection!");
+        println!("Please run \"init\" in this or the appropriate parent directory.");
     }
 
     Ok(())
